da03b6877db86e78202306e13af1633f1e83c477
Enzolm <86968336+Enzolm@users.noreply.github.com>
2024-10-10 10:33:19 +0200

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors("*"));
-
-app.listen(3080, () => {
-  console.log("Serveur à l'écoute sur le port 3080");
-});
